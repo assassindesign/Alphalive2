76406ef89560df41ed0b9b5085eb0e8dbe8aa3f4
Luke Saxton <luke@nu-desine.com>
2017-02-14 16:12:04 +0000

    Greentext pt.3 - More Licenses

--- Threading/RealtimeMessageManager.hpp
+++ Threading/RealtimeMessageManager.hpp
@@ -5,6 +5,17 @@
 //  Created by Luke Saxton on 01/02/2016.
 //
 //
+//  Alphalive 2 is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License, version 2,
+//  as published by the Free Software Foundation.
+//
+//  Alphalive 2 is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef RealtimeMessageManager_hpp
 #define RealtimeMessageManager_hpp
